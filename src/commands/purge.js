f593c264db2abdee67d53fc6809ada356e011f57
XeliteXirish <shaunoneill7@me.com>
2017-02-03 20:02:13 +0000

    Wont delete other users messages

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -3,10 +3,14 @@ exports.run = function (bot, msg, args) {
 
     var deletedMessages = -1;
 
-    msg.channel.fetchMessages({ limit: Math.min(messagecount + 1, 100) })
-        .then(messages => {
-            messages.forEach(m => { m.delete().catch(console.error); deletedMessages++; });
-        }).then(() => {
+    msg.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+        messages.forEach(m => {
+            if (m.author.id == bot.user.id) {
+                m.delete().catch(console.error);
+                deletedMessages++;
+            }
+        });
+    }).then(() => {
         if (deletedMessages === -1) deletedMessages = 0;
         msg.channel.sendMessage(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
             .then(m => m.delete(2000));
