84964fe98aa5a60275e19ae142e67dd50726fd89
XeliteXirish <shaunoneill7@me.com>
2017-02-07 22:42:43 +0000

    Credits

--- src/commands/help.js
+++ src/commands/help.js
@@ -16,7 +16,7 @@ exports.run = function (bot, msg, args) {
         }
 
         msg.edit('', {
-            embed: utils.embed('Commands', 'This message will dissappear in 30 seconds.', fields, { inline: true })
+            embed: utils.embed('Commands', 'This message will dissappear in 30 seconds.', fields, { inline: true }).setFooter("Created by XeliteXirish!")
         }).then(m => m.delete(30000));
 
         return;
