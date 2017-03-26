959acd0c08b9da824705f122aa6f7ad486ff948f
XeliteXirish <shaunoneill7@me.com>
2017-03-26 19:27:27 +0100

    New chatterbot

--- src/commands/cleverbot.js
+++ src/commands/cleverbot.js
@@ -1,8 +1,7 @@
 const utils = require('../utils');
 let enableCleverBot = true;
 
-let cleverbot = require('cleverbot-node');
-let talkbot = new cleverbot();
+var cleverBot;
 
 exports.info = {
     name: 'clever',
@@ -10,7 +9,19 @@ exports.info = {
     description: 'Asks cleverbot a question!'
 };
 
+exports.setup = function (bot) {
+    if (!bot.config.cleverBot_API_USER && !bot.config.cleverBot_API_KEY){
+        console.log("CleverBot API keys are not set, disabling feature");
+        enableCleverBot = false;
+        return;
+    }
+    const cleverbot = require('cleverbot.io');
+    cleverBot = new cleverbot(bot.config.cleverBot_API_USER, bot.config.cleverBot_API_KEY);
+    cleverBot.create();
+}
+
 exports.run = function (bot, msg, args) {
+    if (!enableCleverBot && !cleverBot) return;
 
     if (args.length < 1) {
         msg.edit(':no_entry_sign: You must specify some text!').then(m => m.delete(2000));
@@ -18,7 +29,12 @@ exports.run = function (bot, msg, args) {
     }
 
     let question = args.join(' ');
-    talkbot.write(question, res => {
+    cleverBot.ask(question, (err, res) => {
+        if (err){
+            console.log(`An error occured while retieving data from CleverBot, Error: ${err.stack}`);
+            return;
+        }
+
         console.log(JSON.stringify(res))
         let embed = utils.getSimpleEmbed('', res, utils.randomColor()).setAuthor('Clever Bot', 'http://img.shaunoneill.com/images/2017/03/26/opera_2017-03-26_17-58-34.png').setFooter(utils.randomFooter());
         msg.channel.sendEmbed(embed).then(msg => {
