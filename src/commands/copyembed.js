354ab993f696b6488300fbb8c9e0f90cb26a1668
XeliteXirish <shaunoneill7@me.com>
2017-06-20 01:24:21 +0100

    Update

--- src/commands/copyembed.js
+++ src/commands/copyembed.js
@@ -1,4 +1,5 @@
 const utils = require('../utils');
+const RichEmbed = require('discord.js').RichEmbed;
 
 exports.info = {
     name: 'copyembed',
@@ -7,39 +8,79 @@ exports.info = {
 };
 
 exports.run = function (bot, msg, args) {
+    try {
+        if (args.length > 0) {
 
-    if (args.length > 0) {
+            if (msg.mentions.channels.array().length > 0) {
 
-        if (msg.mentions.channels.array().length > 0) {
-            let messageId = args[0];
+                let messageId = args[0];
 
-            let message = msg.channel.fetchMessage(messageId).then(copied => {
+                msg.channel.fetchMessage(messageId).then(messageCopy => {
+                    if (messageCopy.embeds.length > 0) {
 
-                if (copied.embeds.length > 0) {
-                    msg.mentions.channels.array()[0].send({embed: copied.embeds[0]});
-                }else {
-                    msg.edit(`Sorry but that message doesn't appear to be an embed in that message!`)
-                }
-            }).catch(err => {
-                msg.edit(`Unable to find a message with that id in this channel!`)
-            })
+                        let copied = messageCopy.embeds[0];
 
+                        let embed = new RichEmbed();
+                        if (copied.author) embed.setAuthor(copied.author);
+                        if (copied.color) embed.setColor(copied.color);
+                        if (copied.description) embed.setDescription(copied.description);
+                        copied.fields.forEach(field => {
+                            embed.addField(field);
+                        });
+                        if (copied.footer) embed.setFooter(copied.footer);
+                        if (copied.image) embed.setImage(copied.image);
+                        if (copied.thumbnail) embed.setThumbnail(copied.thumbnail);
+                        if (copied.title) embed.setTitle(copied.title);
+                        if (copied.url) embed.setURL(copied.url);
+
+                        msg.mentions.channels.first().send({embed}).catch(err => {
+                            msg.edit(`Sorry but you need to specify a channel to send the embed too!`)
+                        }).catch(err => {
+                            console.error(`Unable to send embed, Error: ${err.stack}`);
+                        });
+                    } else {
+                        msg.edit(`Sorry but that message doesn't appear to be an embed in that message!`)
+                    }
+                }).catch(err => {
+
+                    msg.edit(`Unable to find a message with that id in this channel!`);
+                })
+            } else {
+
+                let messageId = args[0];
+
+                msg.channel.fetchMessage(messageId).then(messageCopy => {
+                    if (messageCopy.embeds.length > 0) {
+                        let copied = messageCopy.embeds[0];
+
+                        let embed = new RichEmbed();
+                        if (copied.author) embed.setAuthor(copied.author);
+                        if (copied.color) embed.setColor(copied.color);
+                        if (copied.description) embed.setDescription(copied.description);
+                        copied.fields.forEach(field => {
+                            embed.addField(field);
+                        });
+                        if (copied.footer) embed.setFooter(copied.footer);
+                        if (copied.image) embed.setImage(copied.image);
+                        if (copied.thumbnail) embed.setThumbnail(copied.thumbnail);
+                        if (copied.title) embed.setTitle(copied.title);
+                        if (copied.url) embed.setURL(copied.url);
+
+                        msg.channel.send({embed}).catch(err => {
+                            console.error(`Unable to send embed, Error: ${err.stack}`);
+                        });
+                    } else {
+                        msg.edit(`Sorry but that message doesn't appear to be an embed in that message!`);
+                    }
+                }).catch(err => {
+                    msg.edit(`Unable to find a message with that id in this channel!`);
+                    console.log(err.stack)
+                })
+            }
         } else {
-
-            let messageId = args[0];
-
-            let message = msg.channel.fetchMessage(messageId).then(copied => {
-                if (copied.embeds.length > 0) {
-                    msg.mentions.channels.array()[0].send({embed: copied.embeds[0]});
-                }else {
-                    msg.edit(`Sorry but that message doesn't appear to be an embed in that message!`)
-                }
-            }).catch(err => {
-
-                msg.edit(`Unable to find a message with that id in this channel!`)
-            })
+            msg.edit(`You need to specify a message id to search for an embed!`);
         }
-    } else {
-        msg.edit(`You need to specify a message id to search for an embed!`);
+    }catch (err){
+        console.error(`Error running command, Error: ${err.stack}`);
     }
 };
